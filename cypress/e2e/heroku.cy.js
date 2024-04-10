76ccefc3f7ea8416615e7d4152f4a851d51f6406
Mari <maricarmen.jg92@gmail.com>
2024-04-10 20:25:25 +0200

    pruebas de clase

--- cypress/e2e/heroku.cy.js
+++ cypress/e2e/heroku.cy.js
@@ -1,5 +1,5 @@
 describe('template spec', () => {
-  it('test de login valido', () => {
+    it('test de login valido', () => {
     cy.visit('https://the-internet.herokuapp.com')
     cy.get(':nth-child(21) > a').click()
     cy.get('#username').type('tomsmith')
@@ -7,4 +7,19 @@ describe('template spec', () => {
     cy.get('.radius').click()
     cy.get('#flash').contains('You logged into a secure area!')
   })
+  it('test de login invalido', () => {
+    cy.visit('https://the-internet.herokuapp.com')
+    cy.get(':nth-child(21) > a').click()
+    cy.get('#username').type('tomsmith')
+   
+})
+it('test de login contraseña invalida', () => {
+    cy.visit('https://the-internet.herokuapp.com')
+    cy.get(':nth-child(21) > a').click()
+    cy.get('#username').type('tomsmith')
+    cy.get('#password').type('SuperSecretPassword!*')
+  })
+
+
+  
 })
